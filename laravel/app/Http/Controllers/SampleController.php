2271e2ec2a5810249a593376ae0e7364c247702b
bannot <bannot@istyle.co.jp>
2025-02-14 19:51:17 +0900

    add services

--- laravel/app/Http/Controllers/SampleController.php
+++ laravel/app/Http/Controllers/SampleController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Services\SampleService;
 
 class SampleController extends BaseController
 {
-    public function post(): JsonResponse
+    protected $sample;
+
+    public function __construct(
+        SampleService $sample,
+        )
     {
+        $this->sample = $sample;
+    }
+
+    public function post(Request $request): JsonResponse
+    {
+        $month = $request->input('month');
+
         return response()->json(['message' => 'ハロー']);
     }
 }
---
+++ laravel/app/Http/Services/SampleService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use App\Repositories\SampleRepository;
+
+/**
+ * Class SampleService
+ * @package App\Services
+ */
+class SampleService
+{
+    //
+}
